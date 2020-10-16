b11b0a4fa4b031be34b8264155ba381abaece2bb
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-10-16 12:48:31 -0400

    Updates

---
+++ precache-manifest.025e8d5bd54657564a0a15cff96401d3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ba5bb1212ee2fc6f1b89694c15d99d",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3541b879834ec53fad87",
+    "url": "/react-portfolio/static/css/main.262708ba.chunk.css"
+  },
+  {
+    "revision": "2b4c1b1ddd816548a162",
+    "url": "/react-portfolio/static/js/2.c5f5d2bb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.c5f5d2bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3541b879834ec53fad87",
+    "url": "/react-portfolio/static/js/main.df32953f.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "19b652cc26b3cfe03400cfbcc01d5e82",
+    "url": "/react-portfolio/static/media/about-me.19b652cc.jpg"
+  },
+  {
+    "revision": "d477a750f3563b1db90aaea4c565d71f",
+    "url": "/react-portfolio/static/media/code-quiz.d477a750.jpg"
+  },
+  {
+    "revision": "a029f82f57e64629981b2eeaa399b285",
+    "url": "/react-portfolio/static/media/contact-me.a029f82f.jpg"
+  },
+  {
+    "revision": "43140d3df7d4704977f7f83e9f35caff",
+    "url": "/react-portfolio/static/media/gaming-survey.43140d3d.jpg"
+  },
+  {
+    "revision": "0edbc73cdb001a30f1b482eb90def239",
+    "url": "/react-portfolio/static/media/hero.0edbc73c.jpg"
+  },
+  {
+    "revision": "3d19ca620baad61e2b335b1ba06750e4",
+    "url": "/react-portfolio/static/media/minnow.3d19ca62.jpg"
+  },
+  {
+    "revision": "b1243a55124267f080a9d406bdc3854f",
+    "url": "/react-portfolio/static/media/nomadica.b1243a55.jpg"
+  },
+  {
+    "revision": "6cd7b1f471a91cf194dce8a203a25565",
+    "url": "/react-portfolio/static/media/pizza-hunt.6cd7b1f4.jpg"
+  },
+  {
+    "revision": "6fa5a2255a713dadacdd74954bd887b6",
+    "url": "/react-portfolio/static/media/resume.6fa5a225.jpg"
+  },
+  {
+    "revision": "a12989f2cfcc84533837f3fe6f701863",
+    "url": "/react-portfolio/static/media/weather-dashboard.a12989f2.jpg"
+  },
+  {
+    "revision": "a7cfca37371bb882976ca1839d14d0a5",
+    "url": "/react-portfolio/static/media/work.a7cfca37.jpg"
+  }
+]);
