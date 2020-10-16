40210fbb78164bcfe6da3b89152fdc79401a29e2
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-10-15 23:29:12 -0400

    created Contact component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './components/Nav';
 import Footer from './components/Footer';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 
 function App() {
   const tabs = ['About', 'Portfolio', 'Contact', 'Resume'];
@@ -15,6 +16,7 @@ function App() {
       <main>
         <About />
         <Portfolio />
+        <Contact />
       </main>
       <Footer />
     </div>
