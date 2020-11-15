8d3c98defcf54248636d470deb98a1b681fc557a
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-11-14 23:44:21 -0500

    added info to About Me, registered service worker, and installed react-router-dom to create React routes

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,10 +9,26 @@ function About() {
       </div>
       <div className="description">
         <p>
-          Full-Stack Developer based in Orlando, FL with 5 years of business management and sales team leadership experience.
-          Colleagues have described me as driven and adaptable.
+          Adept Full-Stack Developer based in Orlando, FL with 5 years of business management and sales team leadership experience in both professional and private sectors.
+        </p>
+        <br />
+        <p>
+          Endorsed in the ability to accomplish complex assignments ahead of sprint dates through self-management and an empathetic attitude that supports team collaboration and productivity.
+        </p>
+        <br />
+        <p>
+          Colleagues have described me as driven, goal-orientated, adaptable, and an asset to any development team.
+        </p>
+        <br className="desktop-view"/>
+        <p className="desktop-view">
           I ceaselessly strive to learn, improve, and make my days count.
+        </p>
+        <br />
+        <p>
+          Open to new work opportunities.
           Please take a look around and drop me a line if you'd like to work together.
+        </p>
+        <p>
         </p>
       </div>
     </section>
