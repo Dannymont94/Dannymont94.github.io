0fa7ffe0fd1d641271d7525b09127e8af18520a7
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-11-19 00:38:48 -0500

    changed About Me section brand statement

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,22 +13,24 @@ function About() {
         </p>
         <br />
         <p>
-          Endorsed in the ability to accomplish complex assignments ahead of sprint dates through self-management and an empathetic attitude that supports team collaboration and productivity.
+          Recently earned a certificate in full stack development from the University of Central Florida<span className="desktop-view">, where I honed my skills in technologies such as JavaScript, Node.js, React.js, and mobile-first design</span>.
+        </p>
+        <br />
+        <p className="desktop-view">
+          In my most recent project, I worked in a two-person team to develop a MERN SPA that allows users to track their favorite TV shows while under quarantine.
+        </p>
+        <br className="desktop-view"/>
+        <p>
+          Endorsed in the ability to accomplish complex assignments ahead of sprint dates through self-management and an attitude that supports team collaboration and productivity.
         </p>
         <br />
         <p>
-          Colleagues have described me as driven, goal-orientated, adaptable, and an asset to any development team.
-        </p>
-        <br className="desktop-view"/>
-        <p className="desktop-view">
-          I ceaselessly strive to learn, improve, and make my days count.
+          Colleagues have described me as driven, goal-oriented, adaptable, and an asset to any development team.
         </p>
         <br />
         <p>
           Open to new work opportunities.
           Please take a look around and drop me a line if you'd like to work together.
-        </p>
-        <p>
         </p>
       </div>
     </section>
