3daca95a4001c8f950222c43d131bf52432657f4
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-11-20 01:43:05 -0500

    Updates

---
+++ precache-manifest.7ad64cffe69d14259fdc1f60f5909fc9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2ae9fd765e8d756795e6713b114e71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dcd09f1fe741a3dcad2",
+    "url": "/static/css/main.94894879.chunk.css"
+  },
+  {
+    "revision": "36689ca31d253e3146e0",
+    "url": "/static/js/2.087881cd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.087881cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dcd09f1fe741a3dcad2",
+    "url": "/static/js/main.ea7b2546.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "19b652cc26b3cfe03400cfbcc01d5e82",
+    "url": "/static/media/about-me.19b652cc.jpg"
+  },
+  {
+    "revision": "e7ed3ff2cebf2f2b4f63ff72e29325f9",
+    "url": "/static/media/food-festival.e7ed3ff2.jpg"
+  },
+  {
+    "revision": "43140d3df7d4704977f7f83e9f35caff",
+    "url": "/static/media/gaming-survey.43140d3d.jpg"
+  },
+  {
+    "revision": "0edbc73cdb001a30f1b482eb90def239",
+    "url": "/static/media/hero.0edbc73c.jpg"
+  },
+  {
+    "revision": "3d19ca620baad61e2b335b1ba06750e4",
+    "url": "/static/media/minnow.3d19ca62.jpg"
+  },
+  {
+    "revision": "b1243a55124267f080a9d406bdc3854f",
+    "url": "/static/media/nomadica.b1243a55.jpg"
+  },
+  {
+    "revision": "6fa5a2255a713dadacdd74954bd887b6",
+    "url": "/static/media/resume.6fa5a225.jpg"
+  },
+  {
+    "revision": "1e2de7a6343bb1719a9620bd88060087",
+    "url": "/static/media/tv-tracker.1e2de7a6.jpg"
+  },
+  {
+    "revision": "a12989f2cfcc84533837f3fe6f701863",
+    "url": "/static/media/weather-dashboard.a12989f2.jpg"
+  },
+  {
+    "revision": "a7cfca37371bb882976ca1839d14d0a5",
+    "url": "/static/media/work.a7cfca37.jpg"
+  }
+]);
